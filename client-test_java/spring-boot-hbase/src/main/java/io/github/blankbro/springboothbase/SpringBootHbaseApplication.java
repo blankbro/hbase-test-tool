ba104a2e7fdd948985671c731fdf5dd7fdc742ab
lizexin <zexin-li@outlook.com>
2024-01-08 10:42:15 +0800

    client-test_java 项目初始化

---
+++ client-test_java/spring-boot-hbase/src/main/java/io/github/blankbro/springboothbase/SpringBootHbaseApplication.java
@@ -0,0 +1,13 @@
+package io.github.blankbro.springboothbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHbaseApplication.class, args);
+    }
+
+}
---
+++ client-test_java/spring-boot-hbase/src/test/java/io/github/blankbro/springboothbase/SpringBootHbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.blankbro.springboothbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
