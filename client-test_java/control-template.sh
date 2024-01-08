3517374eec0e62fdcf99efbb1704e5b6356fb2a9
lizexin <zexin-li@outlook.com>
2024-01-08 14:40:29 +0800

    add final name

--- client-test_java/control-template.sh
+++ client-test_java/control-template.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-name="spring-boot-hbase"
-jar_name="spring-boot-hbase.jar"
+module_name=$1
+jar_name="$module_name.jar"
 deploy_path="/root/github/hbase-test-tool/client-test_java/output"
 filePah="${deploy_path}/${jar_name}"
 envi="local"
 java_opts=""
 application_properties="--hbase.conf.properties.hbase.zookeeper.quorum= --hbase.conf.properties.zookeeper.znode.parent= "
 Start() {
-    java $java_opts -jar ${filePah} -Dfile.encoding=utf-8 --spring.profiles.active=${envi} ${application_properties} -Djava.security.egd=file:/dev/./urandom --name=${name} &>/dev/null
+    java $java_opts -jar ${filePah} -Dfile.encoding=utf-8 --spring.profiles.active=${envi} ${application_properties} -Djava.security.egd=file:/dev/./urandom --name=${module_name} &>/dev/null
 }
 Stop() {
-    pid=$(ps -ef | grep -n "java.*--name=${name}" | grep -v grep | grep -v kill | awk '{print $2}')
+    pid=$(ps -ef | grep -n "java.*--name=${module_name}" | grep -v grep | grep -v kill | awk '{print $2}')
     if [ ${pid} ]; then
         kill ${tpid}
         for ((i = 0; i < 10; ++i)); do
             sleep 1
-            pid=$(ps -ef | grep -n "java.*--name=${name}" | grep -v grep | grep -v kill | awk '{print $2}')
+            pid=$(ps -ef | grep -n "java.*--name=${module_name}" | grep -v grep | grep -v kill | awk '{print $2}')
             if [ ${tpid} ]; then
                 echo -e ".\c"
             else
@@ -23,7 +23,7 @@ Stop() {
                 break
             fi
         done
-        pid=$(ps -ef | grep -n "java.*--name=${name}" | grep -v grep | grep -v kill | awk '{print $2}')
+        pid=$(ps -ef | grep -n "java.*--name=${module_name}" | grep -v grep | grep -v kill | awk '{print $2}')
         if [ ${pid} ]; then
             echo 'Kill Process!'
             kill -9 ${pid}
@@ -33,7 +33,7 @@ Stop() {
     fi
 
 }
-case $1 in
+case $2 in
 "start")
     Start
     ;;
