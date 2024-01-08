2b87ef8f44796042d192d46abd65fec126b29130
lizexin <zexin-li@outlook.com>
2024-01-08 10:42:36 +0800

    fix import

--- client-test_java/spring-boot-hbase/src/test/java/io/github/blankbro/springboothbase/SpringBootHbaseApplicationTests.java
+++ client-test_java/spring-boot-hbase/src/test/java/io/github/blankbro/springboothbase/SpringBootHbaseApplicationTests.java
@@ -1,6 +1,6 @@
 package io.github.blankbro.springboothbase;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
