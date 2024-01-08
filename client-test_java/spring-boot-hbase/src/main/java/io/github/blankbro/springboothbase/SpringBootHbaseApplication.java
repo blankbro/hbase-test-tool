47bceb58f660c951ac4090abaf36e31daf2c99a7
lizexin <zexin-li@outlook.com>
2024-01-08 14:54:20 +0800

    add log

--- client-test_java/spring-boot-hbase/src/main/java/io/github/blankbro/springboothbase/SpringBootHbaseApplication.java
+++ client-test_java/spring-boot-hbase/src/main/java/io/github/blankbro/springboothbase/SpringBootHbaseApplication.java
@@ -1,13 +1,18 @@
 package io.github.blankbro.springboothbase;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringBootHbaseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootHbaseApplication.class, args);
+        log.info("=========>>> successful <<<=========");
+        log.info("=========>>> successful <<<=========");
+        log.info("=========>>> successful <<<=========");
     }
 
 }
