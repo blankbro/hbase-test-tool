9b831e0ae49f84df7923b1fec0a7835648df8460
lizexin <zexin-li@outlook.com>
2024-01-09 10:29:29 +0800

    print log

--- client-test_java/spring-boot-bigtable/src/main/java/io/github/blankbro/springbootbigtable/controller/TestController.java
+++ client-test_java/spring-boot-bigtable/src/main/java/io/github/blankbro/springbootbigtable/controller/TestController.java
@@ -17,8 +17,18 @@ public class TestController {
     @Autowired
     private HBaseUtils hBaseUtils;
 
+    /**
+     * @param tableName
+     * @param startRow
+     * @param stopRow
+     * @param families
+     * @param qualifiers
+     * @return
+     * @throws IOException
+     */
     @GetMapping("/test-scan")
     public Object testScan(String tableName, String startRow, String stopRow, String[] families, String[] qualifiers) throws IOException {
+        log.info("tableName = {}, startRow = {}, stopRow = {}, families = {}, qualifiers = {}", tableName, startRow, stopRow, families, qualifiers);
         List<Map<String, Object>> maps = hBaseUtils.fuzzyQuery(tableName, startRow, stopRow, families, qualifiers);
         return maps;
     }
