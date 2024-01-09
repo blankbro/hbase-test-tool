7a609247ebbfa465e06fc815a8c8b3374e4fc55b
lizexin <zexin-li@outlook.com>
2024-01-09 10:30:07 +0800

    print log

--- client-test_java/spring-boot-bigtable/src/main/java/io/github/blankbro/springbootbigtable/SpringBootBigtableApplication.java
+++ client-test_java/spring-boot-bigtable/src/main/java/io/github/blankbro/springbootbigtable/SpringBootBigtableApplication.java
@@ -1,13 +1,18 @@
 package io.github.blankbro.springbootbigtable;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringBootBigtableApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootBigtableApplication.class, args);
+        log.info("=========>>> successful <<<=========");
+        log.info("=========>>> successful <<<=========");
+        log.info("=========>>> successful <<<=========");
     }
 
 }
